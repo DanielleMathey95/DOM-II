6f3d642972bb179a0b6743436e6d90471e3c88a3
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-01-16 12:44:49 -0800

    two eventListeners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const funBus = document.querySelector(".intro img")
+funBus.addEventListener("mouseenter", () => {
+  funBus.style.transform = "scale(1.2)";
+  funBus.style.transform = "all 0.3s";
+});//Scales up the header image when mouse is over it
+
+funBus.addEventListener("mouseleave", () => {
+  funBus.style.transform = "scale(1)";
+  funBus.style.transform = "all 0.3s";
+});//Scales header image back down when mouse moves away
+
