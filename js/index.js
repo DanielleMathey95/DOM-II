e5d7dc0edf89676a0794bded81cc65f7e8a5b04a
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-01-16 12:52:19 -0800

    two more events

--- js/index.js
+++ js/index.js
@@ -10,3 +10,14 @@ funBus.addEventListener("mouseleave", () => {
   funBus.style.transform = "all 0.3s";
 });//Scales header image back down when mouse moves away
 
+const changeLogo = document.querySelector('.logo-heading');
+changeLogo.addEventListener('dblclick', () => {
+  changeLogo.style.color = 'green';
+});//Changes color of Fun Bus from black to green when you double click on it
+
+const stopLinks = document.querySelector(".nav")
+stopLinks.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("stopped")
+});//Stops links from refreshing page
+
